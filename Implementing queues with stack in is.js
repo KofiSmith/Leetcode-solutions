8a6da660168ca225a996d0dc939015e6bf4485d8
KofiSmith <72677014+KofiSmith@users.noreply.github.com>
2024-02-27 20:38:53 +0000

    Create Implementing queues with stack in JavaScript.js

---
+++ Implementing queues with stack in is.js
@@ -0,0 +1,35 @@
+function MyQueue(){
+    this.container1 = []
+    this.container2 = []
+    this.push = push
+    this.pop = pop
+    this.peek = peek
+    this.empty = empty
+}
+
+function push(x){
+    
+    while(this.container1.length!=0){
+        this.container2.push(this.container1.pop())
+    }
+    this.container1.push(x)
+    while(this.container2.length !=0){
+        this.container1.unshift(this.container2.shift())
+    }
+}
+
+function pop(){
+    return this.container1.shift()
+}
+function peek(){
+    return this.container1[0]
+}
+
+function empty(){
+    if(this.container1.length==0){
+        return true
+    }else{
+        return false
+    }
+}
+
