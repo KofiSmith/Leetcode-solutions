b0c3b7aa4813aeb44599ab282adcc6972a0e54f6
Kofi Smith <72677014+KofiSmith@users.noreply.github.com>
2025-01-01 21:51:26 +0000

    Add files via upload
    Solution implementation of Leetcode reverse integer problem

---
+++ reverse integer.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def reverse(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        sum = 0
+        sign = -1 if x < 0 else 1
+        x = abs(x)
+
+        while x != 0:
+            rem = x % 10
+            sum = sum * 10 + rem
+            x = x // 10
+
+        sum *= sign
+        
+        # Check for overflow based on 32-bit integer limits
+        if sum > 2**31 - 1 or sum < -2**31:
+            return 0
+
+        return sum
+            
+        
+        
